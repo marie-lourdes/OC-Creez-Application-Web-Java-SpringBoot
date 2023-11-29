f65742dd7e9fe55604266f6cfe4dabfb0ddee73b
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-29 17:40:38 +0100

    EmployeeController.java: add objet model and attribute employee for acces data in view html

--- src/main/java/com/example/webapp/controller/EmployeeController.java
+++ src/main/java/com/example/webapp/controller/EmployeeController.java
@@ -1,14 +1,23 @@
 package com.example.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.webapp.model.Employee;
+import com.example.webapp.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
-
+	@Autowired
+	private EmployeeService service;
+	
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(Model model) {
+    	Iterable<Employee> listEmployee = service.getEmployees();
+		model.addAttribute("employees", listEmployee);
+		return "home";
     }
 
 }
