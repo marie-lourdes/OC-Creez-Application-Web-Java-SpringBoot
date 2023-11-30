e291f73d591413d34e4b1ce22c1d31b7980c79f2
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-30 17:03:56 +0100

    create test integration  EmployeeControllerITTest

---
+++ src/test/java/com/example/webapp/controller/EmployeeControllerITTest.java
@@ -0,0 +1,31 @@
+package com.example.webapp.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@SpringBootTest// charge le contexte d application et les classe reel , pas de mockbean necessaire, different du test unitaire
+@AutoConfigureMockMvc//afin qu’un objet MockMvc soit disponible dans le contexte Spring (ainsi il pourra être injecté dans l’attribut).
+class EmployeeControllerITTest {
+
+	 @Autowired
+	    public MockMvc mockMvc;
+
+	    @Test
+	    public void testGetEmployees() throws Exception {
+	          MvcResult result =mockMvc.perform(get("/"))	
+	            .andDo(print())//imprime l objet retourné, ici la view
+	            .andExpect(status().isOk()).andReturn();
+	           
+	          assertThat(result.getResponse().getContentAsString())
+				.contains(">Laurent</span");
+	    }
+}
